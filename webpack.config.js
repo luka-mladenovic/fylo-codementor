0f0d50b1ee299dfbb9f0efe902aca509284201f4
luka-mladenovic <luka@kreativika.si>
2019-03-31 15:57:26 +0200

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require("path");
+var webpack = require("webpack");
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/styles/main.scss'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.s[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.jpe?g|png|svg$/,
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name]_[hash].[ext]'
+            }
+        }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        filename: '[name].[chunkhash].css'
+    }),
+  ]
+};
